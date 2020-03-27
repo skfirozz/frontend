71381ff0e0aad664b1b6b7afef80065719ff3b02
skfirozz <shaikfiroz838@gmail.com>
2020-03-27 13:05:43 +0530

    [skfirozz]Refactor.modification in display

--- fundoo/src/app/component/displaynotes/displaynotes.component.ts
+++ fundoo/src/app/component/displaynotes/displaynotes.component.ts
@@ -9,116 +9,44 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class DisplaynotesComponent implements OnInit {
 
-  trashedNotes: boolean = false;
-  archiveNotes: boolean = false;
-  trashEmpty: boolean = false;
-  labelNotes: boolean;
-
-  notes: any;
-  getAllNotes: [];
-  private sub: any;
-  private param: any;
-
-  pin: boolean = true;
-  unpin: boolean = true;
-  constructor(private noteservice: NoteservicesService, private route: ActivatedRoute, private router: Router) {
-    this.sub = this.route
-      .queryParams
-      .subscribe(params => {
-        this.param = params['page'] || '';
-        if (this.param == "archive") {
-          console.log("archive");
-          this.getAllArchiveNotes();
-        }
-        else if (this.param == "trash") {
-          console.log("trash");
-          this.getAllTrashedNotes();
-        }
-        else {
-          console.log("display");
-          this.displayNotes();
-        }
-      });
-  }
+  constructor(private noteservice: NoteservicesService, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
   }
 
 
-  arrayOfColors = [
+  notes = [
     [
       { color: "rgb(255, 179, 255)", name: "pink" ,title: "firoz", description: "from karlapalem",isarchived:"1",istrash:"0"},
       { color: "rgb(255, 255, 128)", name: "darkGolden",title: "ershad", description: "from karlapalem" ,isarchived:"1",istrash:"0"},
       { color: "rgb(200, 232, 104)", name: "yellow" ,title: "shamsheer", description: "from karlapalem",isarchived:"0",istrash:"1"},
-      { color: " rgb(158, 136, 191)", name: "darkYellow" ,title: "mallika", description: "from karlapalem",isarchived:"0",istrash:"1"}
-    ],
-    [
+      { color: " rgb(158, 136, 191)", name: "darkYellow" ,title: "mallika", description: "from karlapalem",isarchived:"0",istrash:"1"},
       { color: "slategray", name: "grey" ,title: "karim", description: "from karlapalem",isarchived:"1",istrash:"0",},
       { color: "rgb(153, 221, 255)", name: "Teal" ,title: "sharmila", description: "from karlapalem",isarchived:"0",istrash:"1"},
       { color: "rgb(203,240,248)", name: "blue" ,title: "budi", description: "from karlapalem",isarchived:"1",istrash:"0"},
       { color: "rgb(174,203,250)", name: "Dark blue" ,title: "shabbir", description: "from karlapalem",isarchived:"1",istrash:"0"},
     ]
   ]
-  onPin(noteId) {
-    console.log("on pin called");
 
-    this.noteservice.pinNotes(noteId).subscribe(response => {
-      if (!this.pin) {
-        this.pin = true;
-      }
-      if (!this.unpin) {
-        this.unpin = true;
-      }
-    },
-      (error: any) => {
-        console.log('error');
-      });
-  }
-
-
-  displayNotes() {
-    this.trashedNotes = false;
-    this.archiveNotes = false;
-    this.labelNotes = false;
-    this.trashEmpty = false;
-
-    console.log("Display Notes Call");
-    this.noteservice.getNotesList().subscribe(message => {
-      this.notes = message.notes;
-      console.log(this.notes);
-    });
-  }
-
-  getAllTrashedNotes() {
-    this.trashedNotes = true;
-    this.archiveNotes = false;
-    this.labelNotes = false;
-
-    console.log("trashed Notes subscribe..");
-    this.noteservice.getTrashedNotesList().subscribe(message => {
-      console.log("trashed Notes subscribe..", message.notes);
-      this.notes = message.notes;
-      console.log("final trsah data" + this.trashedNotes);
-      if (this.notes == null) {
-        this.trashEmpty = true;
-      } else {
-        this.trashEmpty = false;
-      }
-
-    });
-  }
-
-  getAllArchiveNotes() {
-    this.archiveNotes = true;
-    this.trashedNotes = false;
-    this.trashEmpty = false;
-    this.labelNotes = false;
-
-    this.noteservice.getArchiveNotesList().subscribe(message => {
-      this.notes = message.notes;
-      console.log(this.notes);
-    });
-  }
-
+  arrayofColors = [
+    [
+      {color : "rgb(255,255,255)", name : "white"},
+      {color : "rgb(255,0,0)", name : "red"},
+      {color : "rgb(255,165,0)", name : "orange"},
+      {color : "rgb(255,255,0)", name : "yellow"}
+    ],
+    [
+      {color : "rgb(0,128,0)", name : "green"},
+      {color : "rgb(0,128,128)", name : "teal"},
+      {color : "rgb(0,0,255)", name : "blue"},
+      {color : "rgb(0,0,139)", name : "dark blue"}
+    ],
+    [
+      {color : "rgb(128,0,128)", name : "purple"},
+      {color : "rgb(255,192,203)", name : "pink"},
+      {color : "rgb(165,42,42)", name : "brown"},
+      {color : "rgb(128,128,128)", name : "gray"}
+    ]
+  ]
 
 }
