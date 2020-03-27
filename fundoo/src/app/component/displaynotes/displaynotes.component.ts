28c5f19e77a260d28c8d741360f86377727afe99
skfirozz <shaikfiroz838@gmail.com>
2020-03-27 17:19:11 +0530

    [skfirozz]Refactor.modification in display added

--- fundoo/src/app/component/displaynotes/displaynotes.component.ts
+++ fundoo/src/app/component/displaynotes/displaynotes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NoteservicesService } from 'src/app/services/noteservices.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Note } from 'src/app/model/note.model';
 
 @Component({
   selector: 'app-displaynotes',
@@ -9,43 +10,62 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class DisplaynotesComponent implements OnInit {
 
-  constructor(private noteservice: NoteservicesService, private route: ActivatedRoute, private router: Router) {  }
+  note: Note = new Note();
+  pinNotes: Note[];
+  unPinNotes: Note[];
+  allNotes: Note[];
+
+  constructor(private noteservice: NoteservicesService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  allnotes() {
+    this.noteservice.getnotes().subscribe( response => {
+    this.allNotes = response.token;
+  })
+      
+  this.noteservice.getPin().subscribe( response => {
+    this.pinNotes = response.token;
+  })
+  
+  this.noteservice.getUnPin().subscribe( response => {
+    this.unPinNotes = response.token;
+  });
+}
+
+
+
+
   notes = [
-      { color: "rgb(255, 179, 255)", name: "pink" ,title: "firoz", description: "from karlapalem",isarchived:"1",istrash:"0"},
-      { color: "rgb(255, 255, 128)", name: "darkGolden",title: "ershad", description: "from karlapalem" ,isarchived:"1",istrash:"0"},
-      { color: "rgb(200, 232, 104)", name: "yellow" ,title: "shamsheer", description: "from karlapalem",isarchived:"0",istrash:"1"},
-      { color: " rgb(158, 136, 191)", name: "darkYellow" ,title: "mallika", description: "from karlapalem",isarchived:"0",istrash:"1"},
-      { color: "slategray", name: "grey" ,title: "karim", description: "from karlapalem",isarchived:"1",istrash:"0",},
-      { color: "rgb(153, 221, 255)", name: "Teal" ,title: "sharmila", description: "from karlapalem",isarchived:"0",istrash:"1"},
-      { color: "rgb(203,240,248)", name: "blue" ,title: "budi", description: "from karlapalem",isarchived:"1",istrash:"0"},
-      { color: "rgb(174,203,250)", name: "Dark blue" ,title: "shabbir", description: "from karlapalem",isarchived:"1",istrash:"0"}
+    { color: "rgb(255, 179, 255)", name: "pink", title: "firoz", description: "from karlapalem", isarchived: "1", istrash: "0" },
+    { color: "rgb(255, 255, 128)", name: "darkGolden", title: "ershad", description: "from karlapalem", isarchived: "1", istrash: "0" },
+    { color: "rgb(200, 232, 104)", name: "yellow", title: "shamsheer", description: "from karlapalem", isarchived: "0", istrash: "1" },
+    { color: " rgb(158, 136, 191)", name: "darkYellow", title: "mallika", description: "from karlapalem", isarchived: "0", istrash: "1" },
+    { color: "slategray", name: "grey", title: "karim", description: "from karlapalem", isarchived: "1", istrash: "0", },
+    { color: "rgb(153, 221, 255)", name: "Teal", title: "sharmila", description: "from karlapalem", isarchived: "0", istrash: "1" },
+    { color: "rgb(203,240,248)", name: "blue", title: "budi", description: "from karlapalem", isarchived: "1", istrash: "0" },
+    { color: "rgb(174,203,250)", name: "Dark blue", title: "shabbir", description: "from karlapalem", isarchived: "1", istrash: "0" }
   ]
 
   arrayofColors = [
     [
-      {color : "rgb(255,255,255)", name : "white"},
-      {color : "rgb(255,0,0)", name : "red"},
-      {color : "rgb(255,165,0)", name : "orange"},
-      {color : "rgb(255,255,0)", name : "yellow"}
+      { color: "rgb(255,255,255)", name: "white" },
+      { color: "rgb(255,0,0)", name: "red" },
+      { color: "rgb(255,165,0)", name: "orange" },
+      { color: "rgb(255,255,0)", name: "yellow" }
     ],
     [
-      {color : "rgb(0,128,0)", name : "green"},
-      {color : "rgb(0,128,128)", name : "teal"},
-      {color : "rgb(0,0,255)", name : "blue"},
-      {color : "rgb(0,0,139)", name : "dark blue"}
+      { color: "rgb(0,128,0)", name: "green" },
+      { color: "rgb(0,128,128)", name: "teal" },
+      { color: "rgb(0,0,255)", name: "blue" },
+      { color: "rgb(0,0,139)", name: "dark blue" }
     ],
     [
-      {color : "rgb(128,0,128)", name : "purple"},
-      {color : "rgb(255,192,203)", name : "pink"},
-      {color : "rgb(165,42,42)", name : "brown"},
-      {color : "rgb(128,128,128)", name : "gray"}
+      { color: "rgb(128,0,128)", name: "purple" },
+      { color: "rgb(255,192,203)", name: "pink" },
+      { color: "rgb(165,42,42)", name: "brown" },
+      { color: "rgb(128,128,128)", name: "gray" }
     ]
   ]
-
-
-
 }
--- fundoo/src/app/services/noteservices.service.ts
+++ fundoo/src/app/services/noteservices.service.ts
@@ -6,52 +6,31 @@ import { Subject, Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class NoteservicesService {
-  private notesList = new Subject<any>();
-  private pinNoteList = new Subject<any>();
-  private archiveNoteList = new Subject<any>();
-  private trashedNoteList = new Subject<any>();
-  private searchNoteData=new Subject<any>();
-
   baseUrl = 'http://127.0.0.1:8000/api/';
 
   constructor(private httpclient: HttpClient) { }
 
-  createNote(note: any) {
-    console.log(note.value);  
-    let noteId = 10;
-    return this.httpclient.post(this.baseUrl + 'createNote', note);//,{ headers: { Authorization: token } });
+
+
+  CreateNote(arr) {
+    let token = localStorage.getItem('token');
+    return this.httpclient.post<any>(this.baseUrl + "createNote", arr, { headers: { Authorization: token } });
+  }
+  getnotes() {
+    let token = localStorage.getItem('token');
+    return this.httpclient.get<any>(this.baseUrl + "getNotes" , { headers:  localStorage.token } ) ;
   }
 
-  editNotes(arr) {
-    return this.httpclient.post(this.baseUrl + 'editNotes', arr);
+  getPin()
+  {
+    let token =localStorage.getItem('token');
+    return this.httpclient.get<any>(this.baseUrl + "/getPin", { headers:  localStorage.token }  );
   }
-
-  deleteNote(noteId: number) {
-
-    console.log(this.baseUrl + 'deleteNote/' + { noteId } );
-
-    return this.httpclient.delete(this.baseUrl + 'deleteNote/' + { noteId } ) ; 
-  }
-
-  pinNotes(note: any) {
-    console.log("note id----->" + note);
-    return this.httpclient.put(this.baseUrl + '/ispinned/' , note);
-  }
-
-  getNotesList(): Observable<any> {
-    console.log("getNotesList Service");
-    return this.notesList.asObservable();
-  }
-
-  getArchiveNotesList(): Observable<any> {
-    console.log("getArchive Service Get");
-    return this.archiveNoteList.asObservable();
+  getUnPin()
+  {
+    let token =localStorage.getItem('token');
+    return this.httpclient.get<any>(this.baseUrl + "/getUnPin", { headers:  localStorage.token }  );
   }
   
-  getTrashedNotesList(): Observable<any> {
-    console.log("trashNote Service Get");
-    return this.trashedNoteList.asObservable();
-  }
-
 
 }
