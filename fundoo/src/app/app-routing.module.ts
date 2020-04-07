6ef8611bd784b1ea0ec4b3a91f76a08d38758680
skfirozz <shaikfiroz838@gmail.com>
2020-04-07 11:12:19 +0530

    [skfirozz]Refacctor.modification in signout

--- fundoo/src/app/app-routing.module.ts
+++ fundoo/src/app/app-routing.module.ts
@@ -3,13 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 import { ForgotComponent } from './component/forgot/forgot.component';
-import { CreatenoteComponent } from './component/createnote/createnote.component';
 import { HomeComponent } from './component/home/home.component';
-import { TrashComponent } from './component/trash/trash.component';
-import { ReminderComponent } from './component/reminder/reminder.component';
 import { DisplaynotesComponent } from './component/displaynotes/displaynotes.component';
 import { LabelnotesComponent } from './component/labelnotes/labelnotes.component';
-import { SignoutComponent } from './component/signout/signout.component';
 
 
 const routes: Routes = [
@@ -20,8 +16,6 @@ const routes: Routes = [
     path: 'fundoo', component: HomeComponent,
     children: [
       { path: 'notes', component: DisplaynotesComponent },
-      { path: 'trash', component: TrashComponent },
-      { path: 'reminder', component: ReminderComponent },
       { path: 'labels', component: LabelnotesComponent },
 
     ]
