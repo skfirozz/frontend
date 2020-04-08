fce89e39e346da425f9429bdc7179e9f70fcfa7e
skfirozz <shaikfiroz838@gmail.com>
2020-04-08 18:44:46 +0530

    [skfirozz]Refactor.modification in reminder

--- fundoo/src/app/component/reminder/reminder.component.ts
+++ fundoo/src/app/component/reminder/reminder.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl} from '@angular/forms';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NoteservicesService } from 'src/app/services/noteservices.service';
 
 @Component({
   selector: 'app-reminder',
@@ -8,21 +9,28 @@ import {FormControl} from '@angular/forms';
 })
 export class ReminderComponent implements OnInit {
 
-  time:any;
-  repeat:any;
-  constructor() { }
+  time = "8:00 AM";
+  repeat = "daily";
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private noteService: NoteservicesService) { }
   ngOnInit(): void {
   }
 
-  setTime(time){
-    this.time=time;
+  setTime(time) {
+    console.log(this.time);
+    this.time = time;
     console.log(time);
   }
+
   setRepitation(repeat) {
-    this.repeat=repeat;
+    this.repeat = repeat;
     console.log(this.repeat);
   }
 
-  
+  save(date) {
+    
+    console.log(date);
+
+  }
+
 
 }
